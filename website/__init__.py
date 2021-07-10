d4a6b374b4c493191a78058da05e3a43f822b659
Venkhat Balaji <venkhatbalaji1607@gmail.com>
2021-07-10 11:52:46 +0000

    Added RDS connectivity

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "dummy"
+    app.config['MYSQL_HOST'] = 'rds-mysql-venkhat.cia6hocr9uul.us-west-1.rds.amazonaws.com'
+    app.config['MYSQL_USER'] = 'admin'
+    app.config['MYSQL_PASSWORD'] = 'admin1234'
+    app.config['MYSQL_DB'] = 'dev'
+
+    mysql = MySQL(app)
 
     from .views import view
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,30 @@
 from flask import Blueprint,render_template,request,flash,redirect,url_for
+from flask_mysqldb import MySQL
+from . import createApp
 
 auth = Blueprint("auth",__name__)
+app = createApp()
 
-@auth.route("login")
+mysql = MySQL(app)
+
+@auth.route("login", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        email = str(email)
+        password = request.form.get('password')
+        cursor = mysql.connection.cursor()
+        #cursor.execute("SELECT * from test where email='venkatbalaji372@gmail.com'")
+        cursor.execute("select * from test where email = %s", (email,) )
+        records = cursor.fetchone()
+        cursor.close()
+        if not records:
+            return "Email is not available. Please signup!"
+        if records[2] == password:
+            return "Login successful"
+        else:
+            return "Wrong password!"
+        return f"{records}"
     return render_template("login.html")
 
 @auth.route("logout")
@@ -17,12 +38,13 @@ def signup():
         firstName = request.form.get('firsname')
         password = request.form.get('password')
         password1 = request.form.get('password1')
-        if len(firstName) < 4:
-            flash('First name must be greater than 4 character.', category='error')
-        elif password != password1:
-            flash("Passwords don\'t match.", category='error')
-        elif len(password) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            return redirect(url_for('views.homePage'))
+        if password != password1:
+            return f"password dont match!"
+        if len(password) < 9:
+            return f"Password should be greater than 8"
+        cursor = mysql.connection.cursor()
+        cursor.execute('''INSERT into test VALUES (%s,%s,%s) ''', (email,firstName,password))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
     return render_template("signup.html")
