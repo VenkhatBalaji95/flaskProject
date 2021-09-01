6640bbe24d9d267e3e256b2a5cfe3defd201dbcc
Ubuntu <ubuntu@ip-10-0-1-73.ap-south-1.compute.internal>
2021-09-01 15:54:11 +0000

    Updated hostname command

--- pythonFlask/website/views.py
+++ pythonFlask/website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,render_template
-import os
+import subprocess
 
 view = Blueprint("views",__name__)
 
@@ -7,8 +7,8 @@ view = Blueprint("views",__name__)
 def homePage():
     return render_template("home.html")
 
-@auth.route("host")
+@view.route("host")
 def host():
-    command = "hostname"
-    hostName = os.system(command)
-    return hostName
+    output = subprocess.run("hostname", stdout=subprocess.PIPE, text = True)
+    output = output.stdout.strip()
+    return output
