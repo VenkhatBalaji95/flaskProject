9e48d56162983f2bb46cb62989edac4f46ff328a
Venkhat <venkatbalaji372@gmail.com>
2021-10-04 23:16:37 +0530

    Added dynamic inventory script

---
+++ ansible/dynamicInventory.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import boto3,yaml,json
+
+def main ():
+  aws_creds = yaml.load(open("dynamicHosts.yaml", "r"), Loader=yaml.FullLoader)["boto3"]
+  ansible = yaml.load(open("dynamicHosts.yaml", "r"), Loader=yaml.FullLoader)["ansible"]
+  env = yaml.load(open("dynamicHosts.yaml", "r"), Loader=yaml.FullLoader)["env"]
+
+  ec2 = boto3.resource('ec2', region_name=aws_creds["region"], aws_access_key_id=aws_creds["access_key"], aws_secret_access_key=aws_creds["secret_access_key"])
+
+  ansibleFilter = { 'Name': 'tag:{0}'.format(ansible["tag"]), 'Values':[ansible["value"]] }
+  envFilter = { 'Name': 'tag:{0}'.format(env["tag"]), 'Values':[env["value"]] }
+  hosts = []
+  for i in ec2.instances.filter(Filters=[ansibleFilter,envFilter]):
+    if i.public_ip_address is not None:
+      hosts.append(i.public_ip_address)
+  if hosts:
+    inventory = {"{0}".format(ansible["tag"]): hosts}
+    print (json.dumps(inventory))
+  else:
+    print ("No servers matched with Tags '{0}: {1}' and '{2}: {3}'.".format(ansible["tag"],ansible["value"],env["tag"],env["value"]))
+    exit (1)
+
+if __name__ == "__main__":
+  main()
